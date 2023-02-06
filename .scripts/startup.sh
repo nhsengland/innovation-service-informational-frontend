67772bab569cdc64c7f3383c4e94c584a771dd60
Manuel Sousa <manuel.sousa@bjss.com>
2023-02-06 12:34:05 +0000

    Merged PR 16035: re-enable weasyprint and add startup script for webapp dependencies

---
+++ .scripts/startup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+apt-get update
+apt-get -y install libpango-1.0-0 libharfbuzz0b libpangoft2-1.0-0 libffi-dev libjpeg-dev libopenjp2-7-dev
+
+gunicorn --bind=0.0.0.0 --timeout 600 is_homepage.wsgi
