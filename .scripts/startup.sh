b03d2b28724ffae604e12718a78af67d2566c5d7
Manuel Sousa <manuel.sousa@bjss.com>
2023-02-07 12:00:20 +0000

    Merged PR 16057: Add configurable media folder + unbuffer stdout/err

--- .scripts/startup.sh
+++ .scripts/startup.sh
@@ -4,4 +4,4 @@ apt-get update
 apt-get -y install libpango-1.0-0 libharfbuzz0b libpangoft2-1.0-0 libffi-dev libjpeg-dev libopenjp2-7-dev
 
 #gunicorn --bind=0.0.0.0 --timeout 600 is_homepage.wsgi
-python3 manage.py runserver 0.0.0.0:8000
+python3 -u manage.py runserver 0.0.0.0:8000
