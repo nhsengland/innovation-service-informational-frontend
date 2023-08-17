f9209cbd68881596817aa014881072d5d103a4ef
Manuel Sousa - BJSS <115169817+manuelsousa-bjss@users.noreply.github.com>
2023-08-17 17:35:18 +0100

    add fix wagtail import paths to startup (#12)

--- .scripts/startup.sh
+++ .scripts/startup.sh
@@ -6,5 +6,8 @@ apt-get -y install libpango-1.0-0 libharfbuzz0b libpangoft2-1.0-0 libffi-dev lib
 python3 manage.py collectstatic --clear --noinput
 python3 manage.py compress
 
+echo "fixing wagtail import paths"
+for A in $(python -c 'import sys; [print(p) for p in sys.path if "site-packages" in p]') ; do wagtail updatemodulepaths $A ; done
+
 #gunicorn --bind=0.0.0.0 --timeout 600 is_homepage.wsgi
 python3 -u manage.py runserver 0.0.0.0:8000
