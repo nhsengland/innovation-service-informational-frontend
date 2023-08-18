b64192a6ff712751f00d22c3fbcf2a417960ef03
Manuel Sousa - BJSS <115169817+manuelsousa-bjss@users.noreply.github.com>
2023-08-18 10:50:21 +0100

    only change paths we know, speed startup (#15)

--- .scripts/startup.sh
+++ .scripts/startup.sh
@@ -4,7 +4,11 @@ apt-get update
 apt-get -y install libpango-1.0-0 libharfbuzz0b libpangoft2-1.0-0 libffi-dev libjpeg-dev libopenjp2-7-dev
 
 echo "fixing wagtail import paths"
-for A in $(python -c 'import sys; [print(p) for p in sys.path if "site-packages" in p]') ; do wagtail updatemodulepaths $A ; done
+wagtail updatemodulepaths antenv/lib/python3.10/site-packages/wagtail/snippets/tests
+wagtail updatemodulepaths antenv/lib/python3.10/site-packages/wagtail/tests
+wagtail updatemodulepaths antenv/lib/python3.10/site-packages/wagtail_pdf_view
+wagtail updatemodulepaths antenv/lib/python3.10/site-packages/wagtailnhsukfrontend
+echo "i've ended fixing wagtail import paths"
 
 python3 manage.py collectstatic --clear --noinput
 python3 manage.py compress
