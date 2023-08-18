77c8cce93c2daf47069ed63e30a99fc88f83e897
Manuel Sousa <manuel.sousa@bjss.com>
2023-08-18 09:56:59 +0100

    add extra line to startup to see if it finishes

--- .scripts/startup.sh
+++ .scripts/startup.sh
@@ -5,6 +5,7 @@ apt-get -y install libpango-1.0-0 libharfbuzz0b libpangoft2-1.0-0 libffi-dev lib
 
 echo "fixing wagtail import paths"
 for A in $(python -c 'import sys; [print(p) for p in sys.path if "site-packages" in p]') ; do wagtail updatemodulepaths $A ; done
+echo "i've ended fixing wagtail import paths"
 
 python3 manage.py collectstatic --clear --noinput
 python3 manage.py compress
