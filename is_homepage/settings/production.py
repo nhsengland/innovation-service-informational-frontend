737f5630d9504a1886f340f4da0b663351b06ea9
Ricardo Tavares <rjt.dev@outlook.com>
2023-08-28 10:50:55 +0100

    Reverted CSP configurations (as they are not needed) and added some visual improvements

--- is_homepage/settings/production.py
+++ is_homepage/settings/production.py
@@ -7,11 +7,6 @@ ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',') if os.environ.get('AL
 # Static files
 COMPRESS_OFFLINE = True
 
-# CSP policies.
-# MIDDLEWARE = MIDDLEWARE + ['csp.middleware.CSPMiddleware']
-MIDDLEWARE = MIDDLEWARE + ['is_homepage.config.middleware.CSPMiddleware']
-# CSP_FRAME_SRC = ("'self'", "youtube.com", "https://www.youtube.com")
-
 
 try:
     from .local import *
