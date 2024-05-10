ee715a5f1c5d7112fcc09e6dcaeb490451274a2e
Carlos Oliveira <carlosdotoliveira@gmail.com>
2024-05-10 15:48:12 +0100

    feat(168074): implement GA4 as recommended by Google

--- is_homepage/static/js/analytics.js
+++ is_homepage/static/js/analytics.js
@@ -9,14 +9,18 @@ const enableAnalytics = environment_variables.ENABLE_ANALYTICS === "true";
   }
 
   // Hotjar tracking Code.
-  (function (h, o, t, j, a, r) {
-    h.hj = h.hj || function () { (h.hj.q = h.hj.q || []).push(arguments) };
-    h._hjSettings = { hjid: 2499228, hjsv: 6 };
-    a = o.getElementsByTagName('head')[0];
-    r = o.createElement('script'); r.async = 1;
-    r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-    a.appendChild(r);
-  })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+  if (getConsentCookie().analytics) {
+    (function (h, o, t, j, a, r) {
+      h.hj = h.hj || function () { (h.hj.q = h.hj.q || []).push(arguments) };
+      h._hjSettings = { hjid: 2499228, hjsv: 6 };
+      a = o.getElementsByTagName('head')[0];
+      r = o.createElement('script');
+      r.id = 'hj-analytics';
+      r.async = 1;
+      r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+      a.appendChild(r);
+    })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+  }
 
   // Google Analytics.
   const node = document.createElement('script');
