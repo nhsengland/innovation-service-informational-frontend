e9e1c5471f514da03f2b1ac671b1227a61936de0
Manuel Sousa <manuel.sousa@bjss.com>
2024-05-31 17:06:40 +0100

    add proto

--- is_homepage/middleware/fetch_original_host.py
+++ is_homepage/middleware/fetch_original_host.py
@@ -5,5 +5,7 @@ class FetchOriginalHostMiddleware:
   def __call__(self, request):
     if 'HTTP_X_ORIGINAL_HOST' in request.META:
       request.META['HTTP_HOST']=request.META['HTTP_X_ORIGINAL_HOST']
+      if 'HTTP_X_FORWARDED_PROTO' in request.META:
+        request.META['wsgi.url_scheme']=request.META['HTTP_X_FORWARDED_PROTO']
       
     return self.get_response(request)
