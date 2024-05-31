cc9958da59be3ed1223f1729ee329537747abd35
Manuel Sousa <manuel.sousa@bjss.com>
2024-05-31 15:09:00 +0000

    ...

--- is_homepage/settings/production.py
+++ is_homepage/settings/production.py
@@ -7,6 +7,8 @@ ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',') if os.environ.get('AL
 # Static files
 COMPRESS_OFFLINE = True
 
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 try:
     from .local import *
