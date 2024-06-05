f0453fb9f0cf2d26fab8462e4d65037c6e1e88ed
Carlos Oliveira <carlosdotoliveira@gmail.com>
2024-06-05 10:45:07 +0100

    Bug/ga4: fix env variables bug (#55)
    * bug(ga4): fix envirnment bug
    
    * bug(environemnt)
    
    * bug(ga4): environment
    
    * bug(ga4): fix spacing
    
    * bug(ga4): fix env variables bug
    
    * bug(ga4): fix docker env variables

--- is_homepage/config/context_processors/environment.py
+++ is_homepage/config/context_processors/environment.py
@@ -6,6 +6,8 @@ def environment_variables(request):
     return {
         'environment_variables': {
             'APP_PRD_URL': 'https://innovation.nhs.uk',
-            'ENABLE_ANALYTICS': os.environ.get('ENABLE_ANALYTICS') == 'true' or False
+            'ENABLE_ANALYTICS': os.environ.get('ENABLE_ANALYTICS') or False,
+            'GTM_ID': os.environ.get('GTM_ID') or '',
+            'TAG_MEASUREMENT_ID': os.environ.get('TAG_MEASUREMENT_ID') or '',
         }
     }
