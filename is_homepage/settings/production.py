90836306331fb751141749ce5df9234d241341a2
Manuel Sousa <manuel.sousa@bjss.com>
2024-10-17 12:38:07 +0100

    change production cookie settings

--- is_homepage/settings/production.py
+++ is_homepage/settings/production.py
@@ -3,6 +3,8 @@ from .base import *
 DEBUG = False
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',') if os.environ.get('ALLOWED_HOSTS') else []
+SESSION_COOKIE_SAMESITE = 'Strict'
+SESSION_COOKIE_SECURE = True
 
 # Static files
 COMPRESS_OFFLINE = True
